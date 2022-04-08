852c9a7166838605d944b2a1c4a4efd10d13155f
ckim14 <chikakim@gmail.com>
2022-04-07 21:07:12 -0400

    edited html and started js

---
+++ workdayscheduler.js
@@ -0,0 +1,24 @@
+//save to local storage
+//loadtasks from storage
+
+//get current time
+//compare current time with scheduled time
+//if past = grey
+//if present = red
+//if future = green
+
+
+
+//get current date
+var CurrentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+$("#currentDay").append(CurrentDate);
+
+var items = {};
+
+$(".time-div").each(function () {
+  var
+  
+}
+
+
